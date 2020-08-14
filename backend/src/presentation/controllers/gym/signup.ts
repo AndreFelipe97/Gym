9d9a2f3e121ef8cc5e482403e2ba35a94e04aba2
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-14 19:50:18 -0300

    refactor: simplify code

--- backend/src/presentation/controllers/gym/signup.ts
+++ backend/src/presentation/controllers/gym/signup.ts
@@ -4,33 +4,12 @@ import { badRequest } from '../../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.phone) {
-      return badRequest(new MissingParamError('phone'))
-    }
-    if (!httpRequest.body.zipCode) {
-      return badRequest(new MissingParamError('zipCode'))
-    }
-    if (!httpRequest.body.street) {
-      return badRequest(new MissingParamError('street'))
-    }
-    if (!httpRequest.body.number) {
-      return badRequest(new MissingParamError('number'))
-    }
-    if (!httpRequest.body.complement) {
-      return badRequest(new MissingParamError('complement'))
-    }
-    if (!httpRequest.body.neighborhood) {
-      return badRequest(new MissingParamError('neighborhood'))
-    }
-    if (!httpRequest.body.city) {
-      return badRequest(new MissingParamError('city'))
-    }
+    const requiredFields = ['name', 'phone', 'zipCode', 'street', 'number', 'complement', 'neighborhood', 'city', 'state']
 
-    if (!httpRequest.body.state) {
-      return badRequest(new MissingParamError('state'))
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
