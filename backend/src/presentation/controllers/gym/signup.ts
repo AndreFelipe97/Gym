4101ab79347cd155115d17e6e76a03e6e28ac815
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-14 20:56:01 -0300

    feat: ensure SignUpController returns 400 if an invalid cnpj is provided

--- backend/src/presentation/controllers/gym/signup.ts
+++ backend/src/presentation/controllers/gym/signup.ts
@@ -2,8 +2,16 @@ import { HttpResponse, HttpRequest } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
+import { CnpjValidator } from '../../protocols/cnpj-validator'
+import { InvalidParamError } from '../../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
+  private readonly cnpjValidator: CnpjValidator
+
+  constructor (cnpjValidator: CnpjValidator) {
+    this.cnpjValidator = cnpjValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'phone', 'cnpj', 'zipCode', 'street', 'number', 'complement', 'neighborhood', 'city', 'state']
 
@@ -11,6 +19,10 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.cnpjValidator.isValid(httpRequest.body.cnpj)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('cnpj'))
     }
   }
 }
