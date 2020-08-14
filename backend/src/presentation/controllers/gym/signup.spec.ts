d624698ee0d890cadff6ee91812d90e10975d905
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-14 19:08:57 -0300

    test: ensure SignUpController returns an error if no complement is provided

--- backend/src/presentation/controllers/gym/signup.spec.ts
+++ backend/src/presentation/controllers/gym/signup.spec.ts
@@ -95,4 +95,23 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: number'))
   })
+
+  test('should return 400 if no complement is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        phone: 'any_phone',
+        zipCode: 'any_zip_code',
+        street: 'any_street',
+        number: 'any_number',
+        neighborhood: 'any_neighborhood',
+        city: 'any_city',
+        state: 'any_state'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: complement'))
+  })
 })
