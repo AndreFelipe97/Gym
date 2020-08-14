ff0e4c1457c62964f5e9026c1251054559911fa7
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-14 16:55:26 -0300

    test: ensure SignUpController returns an error if no phone is provided

--- backend/src/presentation/controllers/gym/signup.spec.ts
+++ backend/src/presentation/controllers/gym/signup.spec.ts
@@ -19,4 +19,23 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no phone is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        zip_code: 'any_zip_code',
+        street: 'any_street',
+        number: 'any_number',
+        complement: 'any_complement',
+        neighborhood: 'any_neighborhood',
+        city: 'any_city',
+        state: 'any_state'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: phone'))
+  })
 })
