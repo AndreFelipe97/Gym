818f9c0eca603b86c48390428891d902146584f8
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-14 23:33:04 -0300

    feat: ensure SignUpController calls AddGym with correct values

---
+++ backend/src/domain/usecases/add-gym.ts
@@ -0,0 +1,18 @@
+import { GymModel } from '../models/gym-model'
+
+export interface AddGymModel {
+  name: string
+  phone: string
+  cnpj: string
+  zipCode: string
+  street: string
+  number: string
+  complement: string
+  neighborhood: string
+  city: string
+  state: string
+}
+
+export interface AddGym {
+  add: (cnpj: AddGymModel) => GymModel
+}
--- backend/src/presentation/controllers/gym/signup.ts
+++ backend/src/presentation/controllers/gym/signup.ts
@@ -1,12 +1,15 @@
 import { Controller, CnpjValidator, HttpResponse, HttpRequest } from '../../protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, serverError } from '../../helpers/http-helper'
+import { AddGym } from '../../../domain/usecases/add-gym'
 
 export class SignUpController implements Controller {
   private readonly cnpjValidator: CnpjValidator
+  private readonly addGym: AddGym
 
-  constructor (cnpjValidator: CnpjValidator) {
+  constructor (cnpjValidator: CnpjValidator, addGym: AddGym) {
     this.cnpjValidator = cnpjValidator
+    this.addGym = addGym
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -18,11 +21,23 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      const { cnpj } = httpRequest.body
+      const { name, phone, cnpj, zipCode, street, number, complement, neighborhood, city, state } = httpRequest.body
       const isValid = this.cnpjValidator.isValid(cnpj)
       if (!isValid) {
         return badRequest(new InvalidParamError('cnpj'))
       }
+      this.addGym.add({
+        name,
+        phone,
+        cnpj,
+        zipCode,
+        street,
+        number,
+        complement,
+        neighborhood,
+        city,
+        state
+      })
     } catch (error) {
       return serverError()
     }
