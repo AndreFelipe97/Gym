2ba54bba8c207a859d1635974fd9c784eb906e6c
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-14 21:23:28 -0300

    refactor: move server error to a helper method

--- backend/src/presentation/controllers/gym/signup.ts
+++ backend/src/presentation/controllers/gym/signup.ts
@@ -1,8 +1,7 @@
 import { HttpResponse, HttpRequest } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { InvalidParamError } from '../../errors/invalid-param-error'
-import { ServerError } from '../../errors/server-error'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
 import { CnpjValidator } from '../../protocols/cnpj-validator'
 
@@ -27,10 +26,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('cnpj'))
       }
     } catch (error) {
-      return {
-        body: new ServerError(),
-        statusCode: 500
-      }
+      return serverError()
     }
   }
 }
--- backend/src/presentation/helpers/http-helper.ts
+++ backend/src/presentation/helpers/http-helper.ts
@@ -1,6 +1,12 @@
 import { HttpResponse } from '../protocols/http'
+import { ServerError } from '../errors/server-error'
 
 export const badRequest = (error: Error): HttpResponse => ({
   body: error,
   statusCode: 400
+})
+
+export const serverError = (): HttpResponse => ({
+  body: new ServerError(),
+  statusCode: 500
 })
