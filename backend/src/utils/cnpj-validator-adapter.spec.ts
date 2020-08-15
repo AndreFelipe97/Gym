76c866c658967105e6e76c1441628effe7382f95
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-15 13:57:13 -0300

    test: make sure you don't get repeated digits

--- backend/src/utils/cnpj-validator-adapter.spec.ts
+++ backend/src/utils/cnpj-validator-adapter.spec.ts
@@ -6,9 +6,16 @@ describe('CnpjValidator Adapter', () => {
     const isValid = sut.isValid('29840191000100')
     expect(isValid).toBe(false)
   })
+
   test('should return true if validator returns true', () => {
     const sut = new CnpjValidatorAdapter()
     const isValid = sut.isValid('29840191000181')
     expect(isValid).toBe(true)
+  })
+
+  test('should return false if numbers are equal', () => {
+    const sut = new CnpjValidatorAdapter()
+    const isValid = sut.isValid('00000000000000')
+    expect(isValid).toBe(false)
   })
 })
