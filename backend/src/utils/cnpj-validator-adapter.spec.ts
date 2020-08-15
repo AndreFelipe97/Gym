416c345c6e9b7cb7cbda477ca390c90c867d97c6
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-15 13:13:44 -0300

    test: ensure CnpjValidatorAdapter returns true if validator returns true

--- backend/src/utils/cnpj-validator-adapter.spec.ts
+++ backend/src/utils/cnpj-validator-adapter.spec.ts
@@ -1,8 +1,14 @@
 import { CnpjValidatorAdapter } from './cnpj-validator'
+
 describe('CnpjValidator Adapter', () => {
   test('should return false if validator returns false', () => {
     const sut = new CnpjValidatorAdapter()
-    const isValid = sut.isValid('invalid_cnpj')
+    const isValid = sut.isValid('29840191000100')
     expect(isValid).toBe(false)
+  })
+  test('should return true if validator returns true', () => {
+    const sut = new CnpjValidatorAdapter()
+    const isValid = sut.isValid('29840191000181')
+    expect(isValid).toBe(true)
   })
 })
