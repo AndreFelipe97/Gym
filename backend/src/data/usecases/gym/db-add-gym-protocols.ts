bc089c39bc560a753b82b03ec0b63fc2d01df242
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-16 16:30:57 -0300

    feat: ensure DbAddGym calls AddGymRepository with correct values

--- backend/src/data/usecases/gym/db-add-gym-protocols.ts
+++ backend/src/data/usecases/gym/db-add-gym-protocols.ts
@@ -1,2 +1,3 @@
-export * from '../../../domain/usecases/add-gym'
-export * from '../../../domain/models/gym-model'
+/* istanbul ignore next */ export * from '../../../domain/usecases/add-gym'
+/* istanbul ignore next */ export * from '../../../domain/models/gym-model'
+/* istanbul ignore next */ export * from '../../protocols/add-gym-repository'
--- backend/src/data/usecases/gym/db-add-gym.ts
+++ backend/src/data/usecases/gym/db-add-gym.ts
@@ -1,7 +1,14 @@
-import { AddGym, AddGymModel, GymModel } from './db-add-gym-protocols'
+import { AddGym, AddGymModel, AddGymRepository, GymModel } from './db-add-gym-protocols'
 
 export class DbAddGym implements AddGym {
-  async add (gym: AddGymModel): Promise<GymModel> {
+  private readonly addGymRepository: AddGymRepository
+
+  constructor (addGymRepository: AddGymRepository) {
+    this.addGymRepository = addGymRepository
+  }
+
+  async add (gymData: AddGymModel): Promise<GymModel> {
+    await this.addGymRepository.add(gymData)
     return await new Promise(resolve => resolve(null))
   }
 }
