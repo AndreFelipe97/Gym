d05ff90197743044ce941e6e3725743b01b3263a
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-17 09:11:02 -0300

    test: ensure SignUpUserController returns 400 if no coach is provided

--- backend/src/presentation/controllers/user/signup.spec.ts
+++ backend/src/presentation/controllers/user/signup.spec.ts
@@ -69,4 +69,21 @@ describe('SiguUp Controller User', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordHash'))
   })
+
+  test('should return 400 if no coach is provided', () => {
+    const sut = new SignUpUserController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        registration: 'any_registration',
+        passwordHash: 'any_passwordHash',
+        admin: 'any_admin',
+        gym: 'any_gym'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('coach'))
+  })
 })
