3255a8bc7ec49010ed123e071ce6849d4e5e4597
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-17 00:31:03 -0300

    test: ensure SignUpUserController returns 400 if no email is provided

--- backend/src/presentation/controllers/user/signup.spec.ts
+++ backend/src/presentation/controllers/user/signup.spec.ts
@@ -17,4 +17,21 @@ describe('SiguUp Controller User', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpUserController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        registration: 'any_registration',
+        passwordHash: 'any_passwordHash',
+        coach: 'any_coach',
+        admin: 'any_admin',
+        gym: 'any_gym'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
