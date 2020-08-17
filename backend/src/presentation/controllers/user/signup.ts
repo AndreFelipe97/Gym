42ee3b393864e3d4666d41a7de3a6e6ebd9a08ec
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-17 08:19:50 -0300

    refactor: ensure SignUpUserController.handle implement correct interfaces

--- backend/src/presentation/controllers/user/signup.ts
+++ backend/src/presentation/controllers/user/signup.ts
@@ -1,11 +1,14 @@
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+
 export class SignUpUserController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         body: new Error('Missing param: name'),
         statusCode: 400
       }
     }
+
     if (!httpRequest.body.email) {
       return {
         body: new Error('Missing param: email'),
