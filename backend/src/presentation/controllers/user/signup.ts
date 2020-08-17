2c5d7a14846ca5857434b3fe9711a4e6c66868c9
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-17 09:10:44 -0300

    feat: ensure SignUpUserController returns 400 if no coach is provided

--- backend/src/presentation/controllers/user/signup.ts
+++ backend/src/presentation/controllers/user/signup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../../helpers/http-helper'
 
 export class SignUpUserController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'registration', 'passwordHash']
+    const requiredFields = ['name', 'email', 'registration', 'passwordHash', 'coach']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
