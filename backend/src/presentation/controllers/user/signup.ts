7e6ccc62b336e3592a68f027a5fc369f5503b953
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-17 09:01:56 -0300

    feat: ensure SignUpUserController returns 400 if no registration is provided

--- backend/src/presentation/controllers/user/signup.ts
+++ backend/src/presentation/controllers/user/signup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../../helpers/http-helper'
 
 export class SignUpUserController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'registration']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
