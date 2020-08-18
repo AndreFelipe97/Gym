85691e74a2a0772bb8ff630b8f5009d80a30de3f
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-17 21:17:32 -0300

    test: ensure SignUpUserController returns 200 if valid data is provided

--- backend/src/presentation/controllers/user/signup.spec.ts
+++ backend/src/presentation/controllers/user/signup.spec.ts
@@ -272,4 +272,31 @@ describe('SiguUp Controller User', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email',
+        registration: 'any_registration',
+        passwordHash: 'any_passwordHash',
+        coach: 'any_coach',
+        admin: 'any_admin',
+        gym: 'any_gym'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      registration: 'valid_registration',
+      passwordHash: 'valid_passwordHash',
+      coach: 'valid_coach',
+      admin: 'valid_admin',
+      gym: 'valid_gym'
+    })
+  })
 })
