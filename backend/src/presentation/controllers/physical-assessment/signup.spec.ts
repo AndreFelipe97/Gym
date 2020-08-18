70e8fc113660949601a8ecac484f5ff71db1a56c
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-18 17:40:38 -0300

    test: ensure SignUpPhysicalAssessmentController returns 400 if no user is provided

---
+++ backend/src/presentation/controllers/physical-assessment/signup.spec.ts
@@ -0,0 +1,38 @@
+import { SignUpPhysicalAssessmentController } from './signup'
+import { MissingParamError } from '../../errors/missing-param-error'
+interface SutTypes {
+  sut: SignUpPhysicalAssessmentController
+}
+const makeSut = (): SutTypes => {
+  const sut = new SignUpPhysicalAssessmentController()
+
+  return { sut }
+}
+
+describe('Singup Controller PhysicalAssessment', () => {
+  test('should return 400 if no user is provided ', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        weight: 'any_weight',
+        height: 'any_height',
+        rightBiceps: 'any_rightBiceps',
+        leftBiceps: 'any_leftBiceps',
+        rightForearm: 'any_rightForearm',
+        leftForearm: 'any_leftForearm',
+        chest: 'any_chest',
+        waist: 'any_waist',
+        abdomen: 'any_abdomen',
+        rightThigh: 'any_rightThigh',
+        leftThigh: 'any_leftThigh',
+        rightCalf: 'any_rightCalf',
+        leftCalf: 'any_leftCalf',
+        date: 'any_date',
+        responsible: 'any_responsible'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('user'))
+  })
+})
