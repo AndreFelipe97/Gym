9bb1cf5ab4cdc6acfa1f4c4256213c1622f2e1be
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-18 17:49:58 -0300

    test: ensure SignUpPhysicalAssessmentController returns 400 if no abdomen is provided

--- backend/src/presentation/controllers/physical-assessment/signup.spec.ts
+++ backend/src/presentation/controllers/physical-assessment/signup.spec.ts
@@ -243,4 +243,30 @@ describe('Singup Controller PhysicalAssessment', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('waist'))
   })
+
+  test('should return 400 if no abdomen is provided ', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        user: 'any_user',
+        weight: 'any_weight',
+        height: 'any_height',
+        rightBiceps: 'any_rightBiceps',
+        leftBiceps: 'any_leftBiceps',
+        rightForearm: 'any_rightForearm',
+        leftForearm: 'any_leftForearm',
+        chest: 'any_chest',
+        waist: 'any_waist',
+        rightThigh: 'any_rightThigh',
+        leftThigh: 'any_leftThigh',
+        rightCalf: 'any_rightCalf',
+        leftCalf: 'any_leftCalf',
+        date: 'any_date',
+        responsible: 'any_responsible'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('abdomen'))
+  })
 })
