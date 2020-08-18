15a184dae477e36780fa30d6e68f9592f55345e0
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-18 17:40:21 -0300

    feat: ensure SignUpPhysicalAssessmentController returns 400 if no user is provided

---
+++ backend/src/presentation/controllers/physical-assessment/signup.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest } from '../../helpers/http-helper'
+
+export class SignUpPhysicalAssessmentController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['user', 'weight', 'height', 'rightBiceps', 'leftBiceps', 'rightForearm', 'leftForearm', 'chest',
+      'waist', 'abdomen', 'rightThigh', 'leftThigh', 'rightCalf', 'leftCalf', 'date', 'responsible']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+  }
+}
