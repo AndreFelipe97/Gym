783a0f064040c20c732ebebfc3c056818cf83950
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-19 11:17:03 -0300

    feat: ensure ExercisesMongoRepository returns an gym on success

---
+++ backend/src/infra/db/mongodb/exercises-repository/exercises.ts
@@ -0,0 +1,13 @@
+import { AddExercisesRepository } from '../../../../data/protocols/add-exercises-repository'
+import { AddExercisesModel } from '../../../../domain/usecases/add-exercises'
+import { ExercisesModel } from '../../../../domain/models/exercises-model'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class ExercisesMongoRepository implements AddExercisesRepository {
+  async add (exercisesData: AddExercisesModel): Promise<ExercisesModel> {
+    const exercisesCollection = MongoHelper.get_collection('exercises')
+    const result = await exercisesCollection.insertOne(exercisesData)
+    const exercises = result.ops[0]
+    return MongoHelper.map(exercises)
+  }
+}
