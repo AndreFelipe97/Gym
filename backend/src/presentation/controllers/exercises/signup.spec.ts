2e8ec496ed044d9cb2a2f4afcf0bab4292113fcd
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-19 10:17:52 -0300

    test: ensure SignUpExercisesController returns 200 if valid data is provided

--- backend/src/presentation/controllers/exercises/signup.spec.ts
+++ backend/src/presentation/controllers/exercises/signup.spec.ts
@@ -84,4 +84,21 @@ describe('SignUp Exercises Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        muscleGroup: 'valid_muscle_group'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      muscleGroup: 'valid_muscle_group'
+    })
+  })
 })
