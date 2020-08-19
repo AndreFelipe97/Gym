aa5adc0f72d2d9d665073a272daa729c1fa560ba
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-19 12:08:38 -0300

    test: ensure SignUpExerciseSheetController returns 400 if no repetition is provided

--- backend/src/presentation/controllers/exercise-sheet/signup.spec.ts
+++ backend/src/presentation/controllers/exercise-sheet/signup.spec.ts
@@ -27,4 +27,36 @@ describe('Signup Exercise Sheet Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('user'))
   })
+
+  test('should return 400 if no exercise is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        user: 'any_user',
+        repetition: 'any_repetition',
+        amount: 'any_amount',
+        day: 'any_day',
+        responsible: 'any_responsible'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('exercise'))
+  })
+
+  test('should return 400 if no repetition is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        user: 'any_user',
+        exercise: 'any_exercise',
+        amount: 'any_amount',
+        day: 'any_day',
+        responsible: 'any_responsible'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('repetition'))
+  })
 })
