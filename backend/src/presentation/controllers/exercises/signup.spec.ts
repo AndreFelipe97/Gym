13992e18af645c4bb5398e6dc27a777016bcbdba
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-19 10:11:09 -0300

    feat: ensure SignUpExercisesController calls AddExercises with correct values

--- backend/src/presentation/controllers/exercises/signup.spec.ts
+++ backend/src/presentation/controllers/exercises/signup.spec.ts
@@ -1,14 +1,31 @@
 import { SignUpExercisesController } from './signup'
 import { MissingParamError } from '../../errors'
+import { AddExercises, AddExercisesModel, ExercisesModel } from './signup-protocols'
+
+const makeAddExercises = (): AddExercises => {
+  class AddExercisesStub implements AddExercises {
+    async add (exercises: AddExercisesModel): Promise<ExercisesModel> {
+      const fakeExercise = {
+        id: 'valid_id',
+        name: 'valid_name',
+        muscleGroup: 'valid_muscle_group'
+      }
+      return await new Promise(resolve => resolve(fakeExercise))
+    }
+  }
+  return new AddExercisesStub()
+}
 
 interface SutTypes {
   sut: SignUpExercisesController
+  addExercisesStub: AddExercises
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new SignUpExercisesController()
+  const addExercisesStub = makeAddExercises()
+  const sut = new SignUpExercisesController(addExercisesStub)
 
-  return { sut }
+  return { sut, addExercisesStub }
 }
 
 describe('SignUp Exercises Controller', () => {
@@ -34,5 +51,21 @@ describe('SignUp Exercises Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('muscleGroup'))
+  })
+
+  test('should call AddExercises with correct values', async () => {
+    const { sut, addExercisesStub } = makeSut()
+    const addSpy = jest.spyOn(addExercisesStub, 'add')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        muscleGroup: 'any_muscle_group'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      muscleGroup: 'any_muscle_group'
+    })
   })
 })
