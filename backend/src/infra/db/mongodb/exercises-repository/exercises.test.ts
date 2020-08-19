114549b3b6b9e5c8551210c7dc9833581fa290e8
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-19 11:17:18 -0300

    test: ensure ExercisesMongoRepository returns an gym on success

---
+++ backend/src/infra/db/mongodb/exercises-repository/exercises.test.ts
@@ -0,0 +1,33 @@
+import { ExercisesMongoRepository } from './exercises'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Exercises Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    const gymCollection = MongoHelper.get_collection('gyms')
+    await gymCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  const makeSut = (): ExercisesMongoRepository => {
+    return new ExercisesMongoRepository()
+  }
+
+  test('should return an exercises on success', async () => {
+    const sut = makeSut()
+    const exercises = await sut.add({
+      name: 'any_name',
+      muscleGroup: 'any_mascle_group'
+    })
+    expect(exercises).toBeTruthy()
+    expect(exercises.id).toBeTruthy()
+    expect(exercises.name).toBe('any_name')
+    expect(exercises.muscleGroup).toBe('any_mascle_group')
+  })
+})
