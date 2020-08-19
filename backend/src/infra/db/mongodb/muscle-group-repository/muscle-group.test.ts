f6084605da5217e7905f395e52d8041bf6227e0c
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-18 22:23:07 -0300

    test: ensure UserMongoRepository returns an muscle group on success

---
+++ backend/src/infra/db/mongodb/muscle-group-repository/muscle-group.test.ts
@@ -0,0 +1,31 @@
+import { MuscleGroupMongoRepository } from './muscle-group'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Physical Assessment mongo repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    const gymCollection = MongoHelper.get_collection('gyms')
+    await gymCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  const makeSut = (): MuscleGroupMongoRepository => {
+    return new MuscleGroupMongoRepository()
+  }
+
+  test('should return an Muscle Group on success', async () => {
+    const sut = makeSut()
+    const muscleGroup = await sut.add({
+      name: 'valid_name'
+    })
+    expect(muscleGroup).toBeTruthy()
+    expect(muscleGroup.id).toBeTruthy()
+    expect(muscleGroup.name).toBe('valid_name')
+  })
+})
