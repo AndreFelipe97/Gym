dec013f94f84338346e6438ca06a29e87724336e
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-19 21:41:54 -0300

    feat: add user factory

---
+++ backend/src/main/factories/user.ts
@@ -0,0 +1,14 @@
+import { SignUpUserController } from '../../presentation/controllers/user/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddUser } from '../../data/usecases/user/db-add-user'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { UserMongoRepository } from '../../infra/db/mongodb/user-repository/user'
+
+export const makeSignUpUserController = (): SignUpUserController => {
+  const salt = 12
+  const emailValidator = new EmailValidatorAdapter()
+  const userMongoRepository = new UserMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const addUser = new DbAddUser(bcryptAdapter, userMongoRepository)
+  return new SignUpUserController(emailValidator, addUser)
+}
