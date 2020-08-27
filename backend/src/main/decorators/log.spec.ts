058263c4b13750223083a66a007a4cf627ccee51
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-27 20:55:31 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- backend/src/main/decorators/log.spec.ts
+++ backend/src/main/decorators/log.spec.ts
@@ -54,4 +54,32 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        registration: 'any_registration',
+        passwordHash: 'any_passwordHash',
+        coach: 'any_coach',
+        admin: 'any_admin',
+        gym: 'any_gym'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        registration: 'any_registration',
+        passwordHash: 'any_passwordHash',
+        coach: 'any_coach',
+        admin: 'any_admin',
+        gym: 'any_gym'
+      }
+    })
+  })
 })
