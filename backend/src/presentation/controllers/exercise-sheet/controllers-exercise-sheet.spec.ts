66e7fc95eefebe846c814434aa15cb8835067ae9
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-27 22:18:43 -0300

    refactor: return null

--- backend/src/presentation/controllers/exercise-sheet/controllers-exercise-sheet.spec.ts
+++ backend/src/presentation/controllers/exercise-sheet/controllers-exercise-sheet.spec.ts
@@ -170,7 +170,7 @@ describe('Signup Exercise Sheet Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError(null))
   })
 
   test('should return 200 if valid data is provided', async () => {
