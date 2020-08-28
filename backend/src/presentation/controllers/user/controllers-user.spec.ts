1f4f121292067de1e581371aa32cafc19e8a94ac
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-08-27 21:45:22 -0300

    refactor: remove duplicated code

--- backend/src/presentation/controllers/user/controllers-user.spec.ts
+++ backend/src/presentation/controllers/user/controllers-user.spec.ts
@@ -1,7 +1,8 @@
 import { UserController } from './controllers-user'
 import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
 import { EmailValidator } from '../../protocols/email-validator'
-import { AddUser, AddUserModel, UserModel } from './protocols'
+import { AddUser, AddUserModel, UserModel, HttpRequest } from './protocols'
+import { successRequest, badRequest, serverError } from '../../helpers/http-helper'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -15,21 +16,34 @@ const makeEmailValidator = (): EmailValidator => {
 const makeAddUser = (): AddUser => {
   class AddUserStub implements AddUser {
     async add (user: AddUserModel): Promise<UserModel> {
-      const fakeUser = {
-        id: 'valid_id',
-        name: 'valid_name',
-        email: 'valid_email',
-        registration: 'valid_registration',
-        passwordHash: 'valid_passwordHash',
-        coach: 'valid_coach',
-        admin: 'valid_admin',
-        gym: 'valid_gym'
-      }
-      return await new Promise(resolve => resolve(fakeUser))
+      return await new Promise(resolve => resolve(makeFakeAddUser()))
     }
   }
   return new AddUserStub()
 }
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email',
+    registration: 'any_registration',
+    passwordHash: 'any_passwordHash',
+    coach: 'any_coach',
+    admin: 'any_admin',
+    gym: 'any_gym'
+  }
+})
+
+const makeFakeAddUser = (): UserModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email',
+  registration: 'valid_registration',
+  passwordHash: 'valid_passwordHash',
+  coach: 'valid_coach',
+  admin: 'valid_admin',
+  gym: 'valid_gym'
+})
 
 interface SutTypes {
   sut: UserController
@@ -62,8 +76,7 @@ describe('SiguUp Controller User', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
   })
 
   test('should return 400 if no email is provided', async () => {
@@ -79,8 +92,7 @@ describe('SiguUp Controller User', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('should return 400 if no registration is provided', async () => {
@@ -96,8 +108,7 @@ describe('SiguUp Controller User', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('registration'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('registration')))
   })
 
   test('should return 400 if no passwordHash is provided', async () => {
@@ -113,8 +124,7 @@ describe('SiguUp Controller User', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('passwordHash'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('passwordHash')))
   })
 
   test('should return 400 if no coach is provided', async () => {
@@ -130,8 +140,7 @@ describe('SiguUp Controller User', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('coach'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('coach')))
   })
 
   test('should return 400 if no admin is provided', async () => {
@@ -147,8 +156,7 @@ describe('SiguUp Controller User', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('admin'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('admin')))
   })
 
   test('should return 400 if no gym is provided', async () => {
@@ -164,43 +172,21 @@ describe('SiguUp Controller User', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('gym'))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('gym')))
   })
 
   test('should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'invalid_email',
-        registration: 'any_registration',
-        passwordHash: 'any_passwordHash',
-        coach: 'any_coach',
-        admin: 'any_admin',
-        gym: 'any_gym'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 
   test('should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut()
     const emailValidator = jest.spyOn(emailValidatorStub, 'isValid')
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email',
-        registration: 'any_registration',
-        passwordHash: 'any_passwordHash',
-        coach: 'any_coach',
-        admin: 'any_admin',
-        gym: 'any_gym'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(emailValidator).toHaveBeenCalledWith('any_email')
   })
@@ -210,36 +196,15 @@ describe('SiguUp Controller User', () => {
     jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
       throw new Error()
     })
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email',
-        registration: 'any_registration',
-        passwordHash: 'any_passwordHash',
-        coach: 'any_coach',
-        admin: 'any_admin',
-        gym: 'any_gym'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 
   test('should call add User with correct values', async () => {
     const { sut, addUserStub } = makeSut()
     const addSpy = jest.spyOn(addUserStub, 'add')
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email',
-        registration: 'any_registration',
-        passwordHash: 'any_passwordHash',
-        coach: 'any_coach',
-        admin: 'any_admin',
-        gym: 'any_gym'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(addSpy).toHaveBeenCalledWith({
       name: 'any_name',
@@ -257,46 +222,15 @@ describe('SiguUp Controller User', () => {
     jest.spyOn(addUserStub, 'add').mockImplementationOnce(async () => {
       return await new Promise((resolve, reject) => reject(new Error()))
     })
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email',
-        registration: 'any_registration',
-        passwordHash: 'any_passwordHash',
-        coach: 'any_coach',
-        admin: 'any_admin',
-        gym: 'any_gym'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 
   test('should return 200 if valid data is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'invalid_email',
-        registration: 'any_registration',
-        passwordHash: 'any_passwordHash',
-        coach: 'any_coach',
-        admin: 'any_admin',
-        gym: 'any_gym'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body).toEqual({
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email',
-      registration: 'valid_registration',
-      passwordHash: 'valid_passwordHash',
-      coach: 'valid_coach',
-      admin: 'valid_admin',
-      gym: 'valid_gym'
-    })
+    expect(httpResponse).toEqual(successRequest(makeFakeAddUser()))
   })
 })
